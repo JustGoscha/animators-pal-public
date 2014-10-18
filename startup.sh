6750480253f41c3f16acb70d82b51114dae44bf0
justgoscha <justgoscha@googlemail.com>
2014-10-18 13:51:44 +0200

    remove logs before starting

--- startup.sh
+++ startup.sh
@@ -4,5 +4,11 @@
 # -l : forever logfile
 # -e : error log
 # --spinSleepTime :time between restarts
+
 LOGDIR=/opt/node-servers/animator-pal/logs
+
+rm ${LOGDIR}/forever.log
+rm ${LOGDIR}/error.log
+rm ${LOGDIR}/server.log
+
 forever -o ${LOGDIR}/server.log -l ${LOGDIR}/forever.log -e ${LOGDIR}/error.log --spinSleepTime 5000 start main.js 
