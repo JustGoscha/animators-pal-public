ea964865ba64333a15b04ca90adeed89f5227035
justgoscha <justgoscha@googlemail.com>
2014-10-18 13:23:20 +0200

    Changed startup script to log error, forever log, and with time between restarts 5 sec

--- startup.sh
+++ startup.sh
@@ -1,1 +1,7 @@
-forever -o /opt/node-servers/animator-pal/server.log main.js > /dev/null &
+#!/bin/bash
+
+# -o : stdout logfile
+# -l : forever logfile
+# -e : error log
+# --spinSleepTime :time between restarts
+forever start main.js -o /opt/node-servers/animator-pal/server.log -l forever.log -e error.log --spinSleepTime 5000
