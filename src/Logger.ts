3feb06db8c9944de9756234969f9664052de01e8
Goscha Graf <justgoscha@gmail.com>
2019-02-19 14:03:07 -0600

    Remove new line from log

--- src/Logger.ts
+++ src/Logger.ts
@@ -19,18 +19,17 @@ export class Logger implements ILogger {
   error(msg: string): void {
     console.error(this.transform(msg))
   }
-  
+
   warn(msg: string): void {
     console.warn(this.transform(msg))
   }
-  
 
   private transform = (message: string) => {
     let logMessage = ""
 
     var date = new Date()
     var date_string = date.toISOString()
-    logMessage += date_string + " | " + message + "\n"
+    logMessage += date_string + " | " + message
     return logMessage
   }
 }
