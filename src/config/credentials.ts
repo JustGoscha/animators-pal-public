9cf862943fa7b89b5afecc61790c9fc7c1789fa7
justgoscha <justgoscha@gmail.com>
2023-08-15 13:13:07 +0200

    Add back stripped credentials file

---
+++ src/config/credentials.ts
@@ -0,0 +1,10 @@
+import { Options } from "twit"
+
+// --------- test credentials ---------
+export const screen_name = "@USERNAME"
+export const credentials: Readonly<Options> = {
+  consumer_key: "CONSUMER_KEY",
+  consumer_secret: "CONSUMER_SECRET",
+  access_token: "ACCESS_TOKEN",
+  access_token_secret: "ACCESS_TOKEN_SECRET",
+}
